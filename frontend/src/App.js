54fc07a856b8696d69a4adbbb3d712785655dc12
sardanad <dhruvsardana2602@gmail.com>
2025-11-28 20:02:24 -0500

    Trying Hash router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/LogInScreen/login";
 import HomeScreen from "./pages/HomeScreen/homeScreen";
 import ShowHistory from "./pages/ShowHistoryScreen/showHistory";
