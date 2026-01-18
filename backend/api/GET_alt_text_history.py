7605456d50798ee541c7d647a6ee8cda870ba6e4
MolyMikhail <Mikham16@mcmaster.ca>
2026-01-17 20:10:28 -0500

    Added draft for get history api and post alt text generation api

---
+++ backend/api/GET_alt_text_history.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+from django.http import HttpResponse, JsonResponse
+
+from backend_controller import backend_controller
+
+#generate alt text api 
+def get_history(request):
+
+    #API endpoint should only be used in get requests
+    if request.method != 'GET':
+        response = JsonResponse(
+            {"error": "Only GET Methods allowed on this endpoint"},
+            status = 405
+        )
+        return response
+    if backend_controller.get_alt_text_history():
+         return HttpResponse(status=200)
+    else:
+        response = JsonResponse(
+            {
+                "error": "NO_HISTORY_FOUND"
+            },
+            status = 400
+        )
+        return response
+
+
+
+    
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .POST_upload import validate_image_api, csrf
+from .POST_gen_alt_text import gen_alt_text_api
+from .GET_alt_text_history import get_history
 
 urlpatterns = [
     path("upload/", validate_image_api),
+    path("generate-alt-text/",gen_alt_text_api),
+    path("alt-text-history/", get_history),
     path("cookie/", csrf)
 ]
