ea1b9f2683e2ddc859a2ad91e69181add6e9e841
fsolier <bulacc1@mcmaster.ca>
2026-01-19 10:52:35 -0500

    add fixes to avoid crashing (refs #335)

--- ai/train/base_train_tester2.py
+++ ai/train/base_train_tester2.py
@@ -3,6 +3,8 @@ import torch
 from PIL import Image
 from datasets import load_dataset
 from transformers import AutoProcessor, BlipForConditionalGeneration, TrainingArguments, Trainer, default_data_collator
+import torch
+
 
 
 '''
@@ -27,12 +29,17 @@ MODEL_ID = "Salesforce/blip-image-captioning-base"
 BASE_DIR = "ai/train/"
 TRAIN_CSV = os.path.join(BASE_DIR, "train.csv")
 VAL_CSV = os.path.join(BASE_DIR, "val.csv")
-OUT_DIR = "ai/train/model/"
+OUT_DIR = "ai/train/model3/"
 
 DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
+torch.backends.mps.enable_fallback = True
 
 processor = AutoProcessor.from_pretrained(MODEL_ID)
 model = BlipForConditionalGeneration.from_pretrained(MODEL_ID).to(DEVICE)
+ 
+# for param in model.vision_model.parameters():
+#     param.requires_grad = False
+
 
 ds = load_dataset("csv", data_files={"train": TRAIN_CSV, "validation": VAL_CSV})
 
@@ -67,13 +74,13 @@ args = TrainingArguments(
     output_dir=OUT_DIR,
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1,
-    gradient_accumulation_steps=8,
+    gradient_accumulation_steps=1,
     learning_rate=5e-5,
     num_train_epochs=3,
     warmup_ratio=0.05,
     eval_strategy="epoch",
     save_strategy="no",
-    logging_steps=25,
+    logging_steps=10,
     remove_unused_columns=False,
     fp16=False,
     dataloader_num_workers=0,
