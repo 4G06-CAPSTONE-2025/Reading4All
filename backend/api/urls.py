29ba5e1c0c4d704a15089487d8fd4f3defba1544
sardanad <dhruvsardana2602@gmail.com>
2026-01-22 01:50:59 -0500

    pylint fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ from .GET_alt_text_history import get_history
 
 urlpatterns = [
     path("upload/", validate_image_api),
-    path("generate-alt-text/",gen_alt_text_api),
+    path("generate-alt-text/", gen_alt_text_api),
     path("alt-text-history/", get_history),
-    path("cookie/", csrf)
+    path("cookie/", csrf),
 ]
