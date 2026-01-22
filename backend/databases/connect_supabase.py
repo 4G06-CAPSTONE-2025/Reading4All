1aeccd47076605206020347b1841b710806c8e38
sardanad <dhruvsardana2602@gmail.com>
2026-01-22 15:16:50 -0500

    supabase lazy initialization

--- backend/databases/connect_supabase.py
+++ backend/databases/connect_supabase.py
@@ -1,11 +1,18 @@
 import os
 from supabase import create_client
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-
 supabase = None
-if SUPABASE_URL and SUPABASE_KEY:
-    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-else:
-    raise EnvironmentError("Supabase URL or Key not found in environment variables.")
+
+def get_supabase_client():
+    global supabase
+
+    if supabase is None:
+        SUPABASE_URL = os.getenv("SUPABASE_URL")
+        SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+
+        if not SUPABASE_URL or not SUPABASE_KEY:
+            raise EnvironmentError("Supabase URL or Key not found in environment variables.")
+        supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+
+    return supabase
