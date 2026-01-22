ff2b172766bb4e530a3dbb5b0e5834a400ec4d06
sardanad <dhruvsardana2602@gmail.com>
2026-01-22 01:50:45 -0500

    pylint fix, removing trailing erros, fixed using black

--- backend/api/GET_alt_text_history.py
+++ backend/api/GET_alt_text_history.py
@@ -3,28 +3,23 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 from django.http import HttpResponse, JsonResponse
 
-from backend.controller.backend_controller import backend_controller
+from backend.backend_controller import backend_controller
 
-#get alt text history api 
+
+# get alt text history api
 def get_history(request):
 
-    #API endpoint should only be used in get requests
-    if request.method != 'GET':
+    # API endpoint should only be used in get requests
+    if request.method != "GET":
         response = JsonResponse(
-            {"error": "Only GET Methods allowed on this endpoint"},
-            status = 405
+            {"error": "Only GET Methods allowed on this endpoint"}, status=405
         )
         return response
-    
+
     # calls backend_controller in order to reach service
-    # session is hardcoded temporarily 
+    # session is hardcoded temporarily
     if backend_controller.get_alt_text_history(session_id=2026):
-         return HttpResponse(status=200)
+        return HttpResponse(status=200)
     else:
-        response = JsonResponse(
-            {
-                "error": "NO_HISTORY_FOUND"
-            },
-            status = 400
-        )
+        response = JsonResponse({"error": "NO_HISTORY_FOUND"}, status=400)
         return response
--- backend/api/POST_gen_alt_text.py
+++ backend/api/POST_gen_alt_text.py
@@ -3,42 +3,29 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 from django.http import HttpResponse, JsonResponse
 
-from backend.controller.backend_controller import backend_controller
+from backend.backend_controller import backend_controller
 
 
-#generate alt text api 
+# generate alt text api
 def gen_alt_text_api(request):
 
-    #API endpoint should only be used in post requests
-    if request.method != 'POST':
+    # API endpoint should only be used in post requests
+    if request.method != "POST":
         response = JsonResponse(
-            {"error": "Only POST Methods allowed on this endpoint"},
-            status = 405
+            {"error": "Only POST Methods allowed on this endpoint"}, status=405
         )
         return response
-    
 
-    
-    image = request.FILES['image']
+    image = request.FILES["image"]
 
-    #mocking getting a session_id from the table
+    # mocking getting a session_id from the table
     session_id = 2026
-    alt_text = backend_controller.gen_alt_text(image,session_id)
+    alt_text = backend_controller.gen_alt_text(image, session_id)
 
     if alt_text:
-        response =  JsonResponse(
-            {
-                "alt_text": alt_text
-            },
-            status = 200
-        )
+        response = JsonResponse({"alt_text": alt_text}, status=200)
         return response
-    
+
     else:
-        response = JsonResponse(
-            {
-                "error": "ERROR_GENERATING"
-            },
-            status = 400
-        )
+        response = JsonResponse({"error": "ERROR_GENERATING"}, status=400)
         return response
