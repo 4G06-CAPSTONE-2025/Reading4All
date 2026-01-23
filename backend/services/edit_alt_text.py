c2e22e30c30406768f983ec18b56e1d03d0016ac
fsolier <bulacc1@mcmaster.ca>
2026-01-23 01:17:56 -0500

    PUT api service layer draft (refs #352)

---
+++ backend/services/edit_alt_text.py
@@ -0,0 +1,37 @@
+import json
+from services.alt_text_history import AltTextHistory
+
+# get history service to interact with Supabase history table 
+history = AltTextHistory()
+
+
+def edit_alt_text(request):
+    try:
+        # parse request 
+        body = json.loads(request.body)
+
+        image = body.get("image")
+        edited_alt_text = body.get("edited_alt_text")
+
+        # same as in GET_alt_test_hisotry.py
+        session_id = 2026
+
+        if not image or not edited_alt_text:
+            return "INVALID_REQUEST"
+
+        # Add edited alt-text as a new entry in the history table 
+        # Original alt text also stays 
+        success = history.add_alt_text_edit(
+            session_id=session_id,
+            image=image,
+            edited_alt_text=edited_alt_text
+        )
+
+        if not success:
+            return "UNABLE_TO_SAVE"
+
+        return "Success"
+
+    except Exception as e:
+        print("EDIT ALT TEXT CONTROLLER ERROR:", e)
+        return "UNABLE_TO_SAVE"
