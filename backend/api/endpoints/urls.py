916ca6679f18905c863bbbeeb7c6a252b0dfb1fe
sardanad <dhruvsardana2602@gmail.com>
2026-01-24 02:51:00 -0500

    add login, logout, and session status endpoints to API

--- backend/api/endpoints/urls.py
+++ backend/api/endpoints/urls.py
@@ -5,7 +5,9 @@ from api.impl.POST_gen_alt_text import gen_alt_text_api
 from api.impl.POST_upload import csrf, validate_image_api
 from api.impl.POST_signup import signup
 from api.impl.PUT_edit import edit_alt_text
-
+from api.impl.POST_login import login
+from api.impl.POST_logout import logout
+from api.impl.GET_session import session_status
 from ..impl.POST_send_verification import send_verification
 from ..impl.POST_verify import verify
 
@@ -17,5 +19,8 @@ urlpatterns = [
     path("cookie/", csrf),
     path("send-verification/", send_verification),
     path("verify/", verify),
-    path("edit-alt-text/", edit_alt_text)
+    path("edit-alt-text/", edit_alt_text),
+    path("login/", login),
+    path("logout/", logout),
+    path("session/", session_status)
 ]
