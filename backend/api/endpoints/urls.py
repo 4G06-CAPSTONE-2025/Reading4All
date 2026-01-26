47a5f17721cbc0ccfa6d77ed852e29bb3a20c1f3
sardanad <dhruvsardana2602@gmail.com>
2026-01-26 00:38:20 -0500

    remove signup endpoint from URL routing

--- backend/api/endpoints/urls.py
+++ backend/api/endpoints/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from api.impl.GET_alt_text_history import get_history
 from api.impl.POST_gen_alt_text import gen_alt_text_api
 from api.impl.POST_upload import csrf, validate_image_api
-from api.impl.POST_signup import signup
 from api.impl.PUT_edit import edit_alt_text
 from api.impl.POST_login import login
 from api.impl.POST_logout import logout
@@ -15,7 +14,6 @@ urlpatterns = [
     path("upload/", validate_image_api),
     path("generate-alt-text/", gen_alt_text_api),
     path("alt-text-history/", get_history),
-    path("signup/", signup),
     path("cookie/", csrf),
     path("send-verification/", send_verification),
     path("verify/", verify),
