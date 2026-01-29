8c1704bba346c1155c0552642c3934b3593e77e4
MolyMikhail <Mikham16@mcmaster.ca>
2026-01-28 20:09:17 -0500

    adding header with mac logo

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
+import mac_logo from "./mcmaster-logo.png"
+
+export default function Header(){
+
+    return (
+       <header className="Reading4All-header">
+        <img
+        className="mcmaster-logo"
+        src={mac_logo}
+        alt="McMaster University Logo"
+        />
+       </header>
+    );
+}
