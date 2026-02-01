219c331ee0699bff2ade35e23c6287f41dbd8433
MolyMikhail <Mikham16@mcmaster.ca>
2026-02-01 11:34:12 -0500

    adding to header to include routing to history pg from upload page and renaming upload pg url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/LogInScreen/login";
-import HomeScreen from "./pages/HomeScreen/homeScreen";
 import HistoryPage from "./pages/HistoryScreen/HistoryPage";
 import Header from "./components/Header";
-
+import UploadScreen from "./pages/UploadScreen/UploadScreen"
 
 function App() {
   return (
-    <>
-    <Header />
     <Router>
+    <Header />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/main" element={<HomeScreen />} />
+        <Route path="/upload" element={<UploadScreen />} />
         <Route path="/session-history" element={<HistoryPage />} />
       </Routes>
-    </Router>
-    </>
+      </Router>
+
 
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,38 @@
 import "./Header.css";
 import mac_logo from "./mcmaster-logo.png"
+import { useLocation, useNavigate} from "react-router-dom"
 
 export default function Header(){
 
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const currLocation = location.pathname 
+
     return (
        <header className="Reading4All-header">
-        <img
-        className="mcmaster-logo"
-        src={mac_logo}
-        alt="McMaster University Logo"
-        />
+
+        <div classname="header-navigation">
+        { 
+            // only the upload pg should have a header to go to history pg
+            currLocation==="/upload" && (
+                <button
+                    className="history-page-navigation-button" 
+                    onClick={() => navigate("/session-history")}>
+                        View History
+                </button>
+            )
+        }
+        </div>
+
+        <div className="header-logo-side">
+            <img
+            className="mcmaster-logo"
+            src={mac_logo}
+            alt="McMaster University Logo"
+            />
+        </div>
+      
        </header>
     );
 }
