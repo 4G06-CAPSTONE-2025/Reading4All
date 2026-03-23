3be3246d93653bf344227ebb4bebd33631b3b398
MolyMikhail <Mikham16@mcmaster.ca>
2026-03-22 23:50:55 -0400

    build error

--- frontend/src/pages/HistoryScreen/HistoryPage.jsx
+++ frontend/src/pages/HistoryScreen/HistoryPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./HistoryPage.css";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function HistoryPage(){
