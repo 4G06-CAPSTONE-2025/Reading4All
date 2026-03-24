f858a474ac5d84550c213eac0a527790aafabb10
MolyMikhail <Mikham16@mcmaster.ca>
2026-03-23 20:24:34 -0400

    adding back to middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,7 +53,8 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware"
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "api.middleware.SessionAuthMiddleware"
 ]
 ROOT_URLCONF = "backend.urls"
 
