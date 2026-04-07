d55b5373b3b8ea8ea4b5222994fd82372d74498d
MolyMikhail <Mikham16@mcmaster.ca>
2026-04-06 21:27:28 -0400

    trailing white space

--- backend/tests/test_edit_alt_text.py
+++ backend/tests/test_edit_alt_text.py
@@ -98,7 +98,7 @@ def test_edit_alt_text_invalid_missing_alt_text():
     assert result == "INVALID_REQUEST"
     mock_history.return_value.update_edited_alt_text.assert_not_called()
 
-# EDIT-UT5: Tests that when Supabase throws an 
+# EDIT-UT5: Tests that when Supabase throws an
 # exception that the appropriate error is returned
 def test_edit_alt_text_exception_thrown():
     # mock AltTextHistory client as its not being tested
